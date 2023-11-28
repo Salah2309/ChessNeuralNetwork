0e80048e63eade294ea581b0de97effc1087616e
Salah2309 <100492797+Salah2309@users.noreply.github.com>
2023-11-28 05:46:16 -0500

    changed html to get data from JSON!

--- runner.js
+++ runner.js
@@ -1,35 +1,41 @@
 // JavaScript function to assign images to squares
-function assignImagesToSquares() {
-    const squares = document.querySelectorAll('.main-container .square');
-    const initialLayout = [
-        'BR', 'BP', '', '', '', '', 'WP', 'WR',
-        'BN', 'BP', '', '', '', '', 'WP', 'WN',
-        'BB', 'BP', '', '', '', '', 'WP', 'WB',
-        'BQ', 'BP', '', '', '', '', 'WP', 'WQ',
-        'BK', 'BP', '', '', '', '', 'WP', 'WK',
-        'BB', 'BP', '', '', '', '', 'WP', 'WB',
-        'BN', 'BP', '', '', '', '', 'WP', 'WN',
-        'BR', 'BP', '', '', '', '', 'WP', 'WR'
-    ];
 
-    const pieceImages = [
-        './images/BR.png', './images/BN.png', './images/BB.png', './images/BQ.png',
-        './images/BK.png', './images/BP.png', './images/WR.png', './images/WN.png',
-        './images/WB.png', './images/WQ.png', './images/WK.png', './images/WP.png'
-    ];
-    
-    // Assign images based on initialLayout array
+let Layout;
+let result = [64];
+//Gets Layout
+function fetchChessLayout(callback) {
+    fetch('Layout.json')
+      .then(response => response.json())
+      .then(data => {
+        Layout = data.imgs;
+        callback();
+      })
+      .catch(error => console.error('Error fetching JSON:', error));
+}
+
+function assignImagesToSquares() {
+    const squares = document.querySelectorAll('.square');   
+    //2D array -> 1D array
+    let i = 0
+    for (let row = 0; row < Layout.length; row++) {
+        for (let col = 0; col < Layout[row].length; col++) {
+            if(Layout[row][col]){
+                result[i] = (Layout[row][col]);
+                i++;
+            }
+        }
+    }
+    //Layout onto Squares
     squares.forEach((square, index) => {
-        const piece = initialLayout[index];
-        if (piece !== '') {
+        piece = result[index]
+        if (piece !== '-') {
             const img = document.createElement('img');
             img.src = `./images/${piece}.png`;
             img.alt = '';
-            square.appendChild(img);
+            square.appendChild(img)
         }
+        
     });
 }
 
-
-// Run the function when the content is loaded
-document.addEventListener('DOMContentLoaded', assignImagesToSquares);
+document.addEventListener('DOMContentLoaded', () => {fetchChessLayout(assignImagesToSquares);});
